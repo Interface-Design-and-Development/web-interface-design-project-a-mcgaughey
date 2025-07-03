e9246e9a7148ebcb63cddb0408ae12cd8a8e9812
Alyssa McGaughey <amcgaughey@twu.edu>
2025-07-02 22:07:17 -0500

    Final Commit

---
+++ webpage/script.js
@@ -0,0 +1,8 @@
+/**
+ * Toggle the visibility of the sidebar on smaller screens.
+ */
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("hide-sidebar");
+  }
+  
